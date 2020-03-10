5635269d1160fb29bafc06eb894f3c8b75c0f7b9
JIANG_YU <y920075@gmail.com>
2020-03-10 12:19:57 +0800

    keyword未完成

--- src/event/event.js
+++ src/event/event.js
@@ -12,13 +12,14 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({extended:false}));
 router.use(bodyParser.json()); 
 
-const del = `DELETE FROM \`weather_data\` WHERE \`classStartDate\` < DATE_FORMAT(NOW(),'%Y-%m-%d')`
-const sql = `SELECT * FROM \`weather_data\` WHERE DATE_FORMAT(\`updated_at\`,'%Y-%m-%d')< DATE_FORMAT(NOW(),'%Y-%m-%d')`
+
 const autoUpdateWeatherData = ()=>{
+    const del = `DELETE FROM \`weather_data\` WHERE \`eventStartDate\` < DATE_FORMAT(NOW(),'%Y-%m-%d')`
+    const sql = `SELECT * FROM \`weather_data\` WHERE DATE_FORMAT(\`updated_at\`,'%Y-%m-%d')< DATE_FORMAT(NOW(),'%Y-%m-%d')`
     console.log('自動更新天氣資訊...')
     setTimeout( async ()=>{
         console.log('刪除已過活動日期資料中...')
-        //await db.queryAsync(del)
+        await db.queryAsync(del)
         console.log('搜索過期資料中...')
         await db.queryAsync(sql)
             .then( async result=>{
@@ -41,8 +42,116 @@ const autoUpdateWeatherData = ()=>{
                 }
             })
         await autoUpdateWeatherData()
-    },6000)
+    },10000)
 }
-autoUpdateWeatherData()
+// autoUpdateWeatherData()
+
+//查詢所有活動列表
+/*
+    預計從前台接收的資料
+
+    GET /event?type=活動類型&sort=排序類型(類型,方法)&page=頁碼&q=關鍵字
+
+    type =    活動類型
+    q =       關鍵字搜索
+    sort =    排序類型  (類型,方法) 
+    page =    頁碼
+
+        預計傳送回去的資料
+    {
+        status =        狀態碼 200=請求成功 404=查無資料
+        msg =           說明訊息
+        searchType =      搜索的活動類型
+        searchKeyword =   搜索的關鍵字
+        sortType =      設定的排序類型 
+        page =          目前頁碼
+        totalRows =     總筆數
+        totalPages =    總頁數
+        result : [
+            {
+                eventId =               活動編號
+                eventName =             活動名稱
+                eventType =             活動類型
+                eventLocation =         活動地點(僅縣市)
+                eventLocation_lat =     活動地點(緯度)
+                eventLocation_lng =     活動地點(經度)
+                eventSponsor =          活動發起人編號
+                loginId =               活動發起人帳號
+                eventStartDate =        活動日期
+                eventEndDate =          報名截止日期
+                eventNeedPeople =       徵求人數
+                eventNowPeople =        現在人數
+                eventImg =              活動圖片
+            }
+        ]
+    }
+*/
+
+router.get('/event',(req,res)=>{
+    const perPage = 8
+    let searchKeyword = ''
+    switch(true){
+        case req.query.q && !req.query.type:
+            searchKeyword = ` WHERE \`event_data\`.\`eventType\` LIKE '%${req.query.q}%' `
+            break;
+        case req.query.q && req.query.type:
+            searchKeyword = ` AND \`event_data\`.\`eventType\` LIKE '%${req.query.q}%' `
+            break;
+    }
+
+    const searchType = req.query.type ? ` WHERE \`event_data\`.\`eventType\` = '${req.query.type}' ` : ''
+    const sort = req.query.sort ? ` ORDER BY \`event_data\`.\`${req.query.sort.split(',')[0]}\` ${req.query.sort.split(',')[1]}` : ` ORDER BY \`event_data\`.\`created_at\` DESC`
+    const total_sql = `SELECT COUNT(1) as 'rows' FROM \`event_data\` ${searchType}${searchKeyword}`
+    let page = req.query.page ? parseInt(req.query.page) : 1
+    let totalRows
+    let totalPages
+
+    db.queryAsync(total_sql)
+    .then(result=>{
+        totalRows = result[0].rows;
+        if ( totalRows===0 ) {
+            return false
+        } else {
+            totalPages = Math.ceil(totalRows/perPage);
+            if (page<1) page=1;
+            if (page>totalPages) page=totalPages;
+
+            const sql = `   SELECT \`event_data\`.\`eventId\`,\`event_data\`.\`eventName\`,\`event_data\`.\`eventType\`,\`event_data\`.\`eventLocation\`,\`event_data\`.\`eventLocation_lat\`,
+                                    \`event_data\`.\`eventLocation_lng\`,\`event_data\`.\`eventSponsor\`,\`event_data\`.\`eventStartDate\`,\`event_data\`.\`eventEndDate\`,
+                                    \`event_data\`.\`eventNeedPeople\`,\`event_data\`.\`eventNowPeople\`,\`event_data\`.\`eventImg\`,\`my_member\`.\`loginId\`
+                                    FROM \`event_data\` 
+                                    LEFT JOIN \`my_member\`
+                                    ON \`event_data\`.\`eventSponsor\` = \`my_member\`.\`memberId\`
+                            ${searchType}${searchKeyword}
+                            ${sort}
+                            LIMIT  ${(page-1)*perPage}, ${perPage}`
+            console.log(sql)
+            return db.queryAsync(sql);
+        }
+    })
+    .then(result=>{
+        if ( result.length>0 ) {
+            res.json({
+                'status' :        200,
+                'msg':            '請求成功',
+                'searchType' :    req.query.type,
+                'searchKeyword' :   req.query.q,
+                'sortType' :      req.query.sort,
+                page,
+                totalRows,
+                totalPages,
+                result
+        })
+        } else {
+            res.json({
+                'status' :        404,
+                'msg':            '查無任何資料',
+                'searchType' :    req.query.type,
+                'searchKeyword' :   req.query.q,
+                'sortType' :      req.query.sort,
+            })
+        }
+    })
+})
 
 module.exports = router
