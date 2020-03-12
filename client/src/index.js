cd376bce5e11e5f652fe22875886b3aea13d5f40
jy0119 <y920075@gmail.com>
2020-03-12 15:00:17 +0800

    react基礎環境建置

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//redux
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+
+//導入reducers
+import { blogReducer } from './reducers/blog/blog_Reducers'
+import { classReducer } from './reducers/class/class_Reducers'
+import { eventReducer } from './reducers/event/event_Reducers'
+import { itemReducer } from './reducers/item/item_Reducers'
+import { memberReducer } from './reducers/member/member_Reducers'
+
+//引入中介軟體
+import thunk from 'redux-thunk'
+
+//合併reducers
+const rootReducer = combineReducers({
+    blogReducer,
+    classReducer,
+    eventReducer,
+    itemReducer,
+    memberReducer,
+})
+
+//使用中介軟體時，建立store的方法，需要額外建立一個composeEnhancers
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(
+  rootReducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(thunk))
+)
+
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
