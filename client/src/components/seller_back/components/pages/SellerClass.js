c5da6c0ccd9084610b253a0a25ea34ab08f6d895
JIANG_YU <y920075@gmail.com>
2020-03-21 23:49:01 +0800

    引入新增的action與store

--- client/src/components/seller_back/components/pages/SellerClass.js
+++ client/src/components/seller_back/components/pages/SellerClass.js
@@ -5,15 +5,25 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 //引入action
-import { getSellerClassDataAsunc } from '../../../../actions/seller/index'
+import {
+  getSellerClassDataAsunc,
+  getCityDataAsunc,
+  getDistDataAsunc,
+  getClassTypeLevelDataForSellerAsunc,
+  addClassDataAsunc,
+} from '../../../../actions/seller/index'
 
 import ManageClassContent from './SellerClassComponents/ManageClassContent'
 import ManageCoachContent from './SellerClassComponents/ManageCoachContent'
 import AddClassContent from './SellerClassComponents/AddClassContent'
 
 function SellerClass(props) {
+  const [nowClickTag, setNowClickTag] = useState(2)
+
   useEffect(() => {
     props.getSellerClassDataAsunc()
+    props.getCityDataAsunc()
+    props.getClassTypeLevelDataForSellerAsunc(true, false)
   }, [])
 
   //向伺服器取得新資料
@@ -23,19 +33,74 @@ function SellerClass(props) {
     props.getSellerClassDataAsunc(sort, page)
   }
 
+  const handleNavActive = event => {
+    const navButtons = document.querySelectorAll('button.nav-item.nav-link')
+    navButtons.forEach((value, index) => {
+      value.classList.remove('active')
+    })
+    event.target.classList.add('active')
+  }
+
   return (
     <>
       <div className="container JY-Seller-Class">
         <nav className="nav nav-pills nav-justified">
-          <button className="nav-item nav-link active">管理課程</button>
-          <button className="nav-item nav-link">新增課程</button>
-          <button className="nav-item nav-link ">管理教練資訊</button>
+          <button
+            className="nav-item nav-link active"
+            onClick={event => {
+              handleNavActive(event)
+              setNowClickTag(1)
+            }}
+          >
+            管理課程
+          </button>
+          <button
+            className="nav-item nav-link"
+            onClick={event => {
+              handleNavActive(event)
+              setNowClickTag(2)
+            }}
+          >
+            新增課程
+          </button>
+          <button
+            className="nav-item nav-link "
+            onClick={event => {
+              handleNavActive(event)
+              setNowClickTag(3)
+            }}
+          >
+            管理教練資訊
+          </button>
         </nav>
         <div className="col-12">
-          <ManageClassContent
-            sellerClassData={props.sellerClassData}
-            getSellerClassData={getSellerClassData}
-          />
+          {(() => {
+            switch (nowClickTag) {
+              case 2:
+                return (
+                  <AddClassContent
+                    cityData={props.cityData}
+                    handleGetDistData={props.getDistDataAsunc}
+                    handleGetLevelData={
+                      props.getClassTypeLevelDataForSellerAsunc
+                    }
+                    distData={props.distData}
+                    typeData={props.typeDataForSeller}
+                    levelData={props.levelDataForSeller}
+                    addClassData={props.addClassDataAsunc}
+                  />
+                )
+              case 3:
+                return <ManageCoachContent />
+              default:
+                return (
+                  <ManageClassContent
+                    sellerClassData={props.sellerClassData}
+                    getSellerClassData={getSellerClassData}
+                  />
+                )
+            }
+          })()}
         </div>
       </div>
     </>
@@ -44,12 +109,28 @@ function SellerClass(props) {
 
 // 取得Redux中store的值
 const mapStateToProps = store => {
-  return { sellerClassData: store.sellerReducer.sellerClassData }
+  return {
+    sellerClassData: store.sellerReducer.sellerClassData,
+    cityData: store.sellerReducer.cityData,
+    distData: store.sellerReducer.distData,
+    typeDataForSeller: store.sellerReducer.typeDataForSeller,
+    levelDataForSeller: store.sellerReducer.levelDataForSeller,
+    addClassDataResponse: store.sellerReducer.addClassDataResponse,
+  }
 }
 
 // 指示dispatch要綁定哪些action creators
 const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ getSellerClassDataAsunc }, dispatch)
+  return bindActionCreators(
+    {
+      getSellerClassDataAsunc,
+      getCityDataAsunc,
+      getDistDataAsunc,
+      getClassTypeLevelDataForSellerAsunc,
+      addClassDataAsunc,
+    },
+    dispatch
+  )
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SellerClass)
