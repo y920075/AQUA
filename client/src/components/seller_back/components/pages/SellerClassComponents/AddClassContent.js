cf3ba1f9afe6e831f43ce98bf351fb74aa10fbd7
JIANG_YU <y920075@gmail.com>
2020-03-21 19:14:03 +0800

    初始化賣家中心元件

---
+++ client/src/components/seller_back/components/pages/SellerClassComponents/AddClassContent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function AddClassContent(props) {
+  return <></>
+}
+
+export default AddClassContent
---
+++ client/src/components/seller_back/components/pages/SellerClassComponents/ManageCoachContent.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ManageCoachContent(props) {
+  return <></>
+}
+
+export default ManageCoachContent
