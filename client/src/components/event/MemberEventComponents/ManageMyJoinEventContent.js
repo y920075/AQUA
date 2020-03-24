36c6fcade12ef86249f56bb5fc7ebad7250fded9
JIANG_YU <y920075@gmail.com>
2020-03-24 11:52:47 +0800

    新增活動以及查詢自己參加的活動元件初始化

---
+++ client/src/components/event/MemberEventComponents/ManageMyJoinEventContent.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import SweetAlert from '../../class/SellerClassComponents/Sweetalert2'
+
+import Loading from '../../class/Loading' //載入中圖示
+import EventPageButtons from '../EventPageButtons'
+
+/*
+  傳入參數
+  
+
+  傳入方法
+  
+
+  
+*/
+
+function ManageMyJoinEventContent(props) {
+  return <>myjoin</>
+}
+
+export default ManageMyJoinEventContent
