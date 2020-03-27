85ee2ce228e5dcc1f38d7015684aa7fd110dace7
Flavius Tullius Valerianus <40697494+TitusFlavius@users.noreply.github.com>
2020-03-27 20:38:22 +0800

    評論更新

---
+++ client/src/actions/comment/comment_action.js
@@ -0,0 +1,69 @@
+import axios from 'axios'
+
+//取得地點留言
+export const getLocationcomment = data => ({
+  type: 'GET_LOCATIONCOMMENT',
+  value: data,
+})
+export const fetchLocationcomment = () => {
+  return dispatch => {
+    axios
+      .get('http://localhost:5000/comment/location')
+      .then(function(response) {
+        // Success
+        const comment = response.data
+        dispatch(getLocationcomment(comment))
+      })
+      .catch(function(error) {
+        // Error
+        console.log(error)
+      })
+  }
+}
+
+//取得文章留言
+export const getBlogcomment = data => ({
+  type: 'GET_BLOGCOMMENT',
+  value: data,
+})
+export const fetchBlogcomment = () => {
+  return dispatch => {
+    axios
+      .get('http://localhost:5000/comment/blog')
+      .then(function(response) {
+        // Success
+        const comment = response.data
+        dispatch(getBlogcomment(comment))
+      })
+      .catch(function(error) {
+        // Error
+        console.log(error)
+      })
+  }
+}
+//送出留言資料
+export const Sentlocationcomment = data => ({
+  type: 'SENT_COMMENT',
+  value: data,
+})
+
+export const Submmitlocationcomment = formData => {
+  return async dispatch => {
+    const fd = new FormData()
+    fd.append('pageid', formData.pageid)
+    fd.append('memberID', formData.memberid)
+    fd.append('membername', formData.membername)
+    fd.append('commenttime', formData.commenttime)
+    fd.append('commentitdelf', formData.commentitdelf)
+    const request = new Request(
+      'http://localhost:5000/comment/sentlocationcomment',
+      {
+        method: 'POST',
+        body: fd,
+      }
+    )
+    const response = await fetch(request)
+    const data = await response.json()
+    dispatch(Sentlocationcomment(data))
+  }
+}
--- server/src/comment/comment.js
+++ server/src/comment/comment.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const moment = require('moment-timezone');
-
-const multer = require("multer");
-const upload = multer({dest: 'tmp_uploads/'});
-
-const fs = require('fs');
-
-const db = require(__dirname + '/../db_connect');
-var async = require('async');
-
+const moment =  require('moment-timezone');
+const multer =  require("multer");
+const upload =  multer({dest: 'tmp_uploads/'});
+const fs =      require('fs');
+const db =      require(__dirname + '/../db_connect');
+var async =     require('async');
 const router = express.Router();
-
-
 const dateFormat = "YYYY-MM-DD";
 
 router.get('/location' , (req, res)=>{
-    const locuscomment = "SELECT * FROM comment";
+    const locuscomment = "SELECT * FROM comment_location";
     db.queryAsync(locuscomment)
     .then(result=>{
         res.json({
@@ -29,10 +23,36 @@ router.get('/blog' , (req, res)=>{
     db.queryAsync(locuscomment)
     .then(result=>{
         res.json({
-            locationcomment: result
+            blogcomment: result
     })
     })
     
 })
-
+//接收留言寫入資料庫
+router.post('/sentlocationcomment',upload.none(),(req,res)=>{
+    // req.session.memberID = ''
+    // const date = {
+    //     'status':401,
+    //     'msg':'尚未登入'
+    // // }
+    // if ( !req.session.memberId ) {
+    //     res.json(data)
+    // } else
+    const sql = "INSERT INTO `comment_location` (`comment_id`,`Locationid`,`member_id`,`member_name`,`comment_date`,`comment`) VALUE (?,?,?,?,?,?)"
+    let commentid = "L" + moment().format('YYYYMMDDHHMMSS')
+    let memberID = 'M20010002'
+    let membername = "Valeriana Flavia"
+    console.log(commentid)
+    let sqlStmt = [
+        commentid,
+        req.params.LocationID,
+        memberID,
+        membername,
+        // req.session.memberID,
+        // req.session.membername,
+        req.body.comment_date,
+        req.body.comment
+    ]
+    db.queryAsync(aql , sqlStmt)
+})
 module.exports = router;
