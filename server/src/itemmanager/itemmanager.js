184d37b360c923149706b1995d026819c2fed171
ChingDa <asdfghjkl123620@gmail.com>
2020-04-05 14:57:40 +0800

    後端商品建立

--- server/src/itemmanager/itemmanager.js
+++ server/src/itemmanager/itemmanager.js
@@ -15,9 +15,24 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({extended:false}));
 router.use(bodyParser.json()); 
 
+//將商品資料傳到賣家前台去
+
 router.get('/item-total-data',(req,res)=>{
 
-    // const item_total = `SELECT * FROM`
+    const item_total = `SELECT * FROM \`items\``
+    
+    const itemData = {
+        transferdata:req.body,
+        success:false,
+        itemsData:""
+    }
+    db.queryAsync(item_total)
+    .then(result=>{
+        itemData.transferdata = req.body
+        itemData.successful = true,
+        itemData.itemsData = result
+    })
+    res.json(data)
 
 })
 
